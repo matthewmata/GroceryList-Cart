b7efd51f13a450d2ccd575a610d1bbeafc05f536
Hack Reactor Student <student@hackreactor.com>
2019-03-23 14:28:33 -0700

    ...

--- mongoServer/controller.js
+++ mongoServer/controller.js
@@ -5,12 +5,40 @@ const controller = {
     get: (req, res) => {
       db.Products.find({})
       .then((data) => {res.status(200).send(data)})
-      .catch((err) => {res.status(404).send(data)})
+      .catch((err) => {res.status(404).send(err)})
     },
     post: (req, res) => {
       const {name, price, quantity} = req.body
       db.Products.create({name, price, quantity})
-      .then(())
+      .then(() => {res.status(201).send()})
+      .catch((err) => {res.status(404).send(err)})
+    },
+    delete: (req, res) => {
+      const { name } = req.query
+      db.Products.deleteOne({ name })
+      .then(() => {res.status(202).send()})
+      .catch((err) => {res.status(404).send(err)})
+    },
+    update: (req, res) => {
+      const { name } = req.body
+
     }
-  }
+  },
+  sales: {
+    get: (req, res) => {
+
+    },
+    post: (req, res) => {
+
+    },
+    delete: (req, res) => {
+
+    },
+    update: (req, res) => {
+
+    }
+  }  
 }
+
+1314
+1642
